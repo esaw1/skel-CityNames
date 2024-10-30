0e13939434c15423a897f26f98734826822ed171
esaw1 <ethansaw@outlook.com>
2024-10-29 17:53:43 -0700

    task 3 attempt

--- src/main/java/citynetwork/CityNetwork.java
+++ src/main/java/citynetwork/CityNetwork.java
@@ -1,7 +1,12 @@
 package citynetwork;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class CityNetwork {
     public final List<CityTLA> _cities = new ArrayList<>();
@@ -31,9 +36,51 @@ public class CityNetwork {
      * @return a {@code CityNetwork} instance with the maximum number of cities from {@code cityNames} that can have ambiguity-free TLAs
      */
     public static final CityNetwork buildNetwork(List<String> cityNames) {
-        if (cityNames.size() < 1) {
-            return null;
+        Map<String, Set<String>> codeMap = new HashMap<>();
+        for (String cityName : cityNames) {
+            codeMap.put(cityName, getCodeSet(cityName));
         }
-        return null;
+
+        CityNetwork cityNet = new CityNetwork();
+        for (int i = 0; i < cityNames.size(); i++) {
+            String cityName1 = cityNames.get(i);
+            boolean valid = true;
+            for (String testCode : codeMap.get(cityName1)) {
+                for (int j = 0; j < cityNames.size(); j++) {
+                    String cityName2 = cityNames.get(j);
+                    if (i != j && Utils.isValidCode(cityName2, testCode)) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid) {
+                    cityNet._cities.add(new CityTLA(cityName1, testCode));
+                    System.out.println(cityName1);
+                    System.out.println(testCode);
+                    break;
+                }
+            }
+            System.out.println();
+        }
+        return cityNet;
+    }
+
+    private static Set<String> getCodeSet(String cityName) {
+        Set<String> possibleCodes = new HashSet<>();
+        for (int start = 0; start < cityName.length() - 2; start++) {
+            for (int end = start + 2; end < cityName.length(); end++) {
+                for (int middle = start + 1; middle < end; middle++) {
+                    String code = cityName.charAt(start) + "" +
+                                  cityName.charAt(middle) +
+                                  cityName.charAt(end);
+                    possibleCodes.add(code);
+                }
+            }
+        }
+        if (possibleCodes.isEmpty()) {
+            return Collections.emptySet();
+        } else {
+            return possibleCodes;
+        }
     }
 }
